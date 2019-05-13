8f03ee3beb1559bf8fa437a1927f4958849eff1b
Pouya Kermanshahi <pouya795@outlook.com>
2019-05-14 00:49:55 +0430

    group app is completed and ready. this closes #11

--- groups/models.py
+++ groups/models.py
@@ -4,16 +4,16 @@ from users.models import Businessman, Customer
 # Create your models here.
 
 
-class BusinessmanCustomersGroups(models.Model):
+class BusinessmanGroups(models.Model):
 
     title = models.CharField(max_length=40)
     create_date = models.DateTimeField(auto_now_add=True)
     businessman = models.ForeignKey(Businessman, on_delete=models.CASCADE)
-    customer = models.ManyToManyField(Customer)
+    customers = models.ManyToManyField(Customer)
 
     def __str__(self):
         return f"{self.title} - {self.businessman.username}"
 
     class Meta:
 
-        db_table = 'businessman_customers_groups'
+        db_table = 'businessman_groups'
