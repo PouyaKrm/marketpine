3e23ac40d1aa7a23943c2199dd56428a648456b4
pouya795 <pouya795@outlook.com>
2019-07-16 23:37:21 +0430

    fixing festival view that adds customer to festival. From now to add a customer to a festival phone number and the discount code of the festival is needed. Also pagination for the list of the customers in festival is added.

---
+++ common/util/paginators.py
@@ -0,0 +1,21 @@
+from django.db.models import QuerySet
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.request import Request
+from rest_framework.serializers import Serializer
+
+
+class NumberedPaginator(PageNumberPagination):
+
+    def __init__(self, page_size_value: int, request: Request, query_set: QuerySet, srl: Serializer.__class__):
+
+        self.srl = srl
+        self.query_set = query_set
+        self.request = request
+        self.page_size = page_size_value
+
+    def next_page(self):
+
+        page_result = self.paginate_queryset(self.query_set, self.request)
+        serializer = self.srl(page_result, many=True)
+
+        return self.get_paginated_response(serializer.data)
--- festivals/urls.py
+++ festivals/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.FestivalAPIView.as_view()),
     path('<int:id>/', views.FestivalRetrieveAPIView.as_view()),
-    path('<int:festival_id>/customers/', views.FestivalCustomerAPIView.as_view()),
+    path('<int:festival_id>/customers/', views.list_customers_in_festival),
+    path('customers/', views.add_customer_to_festival),
     path('<int:festival_id>/customers/<customer_id>/', views.delete_customer_from_festival),
 ]
