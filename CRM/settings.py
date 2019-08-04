6e876dbc2ab3899ae55eaa5e0fdc737fd79caeac
pouya795 <pouya795@outlook.com>
2019-08-04 13:56:45 +0430

    Adding x-api-key header to the list of allowed cors headers.

--- CRM/settings.py
+++ CRM/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 from django.conf import settings
+from corsheaders.defaults import default_headers
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -220,6 +221,9 @@ JWT_AUTH = {
 }
 
 CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_HEADERS = list(default_headers) + [
+    'x-api-key'
+]
 CORS_EXPOSE_HEADERS = [
     'authorization',
     'content-type',
