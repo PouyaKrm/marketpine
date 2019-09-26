3ed1a1f7ad8458a5ceb74e373edae36a1c7156d0
hossein otrsh <boyyoung2020@gmail.com>
2019-09-26 18:29:40 +0330

    fix pagination on views

--- panelmodulus/views.py
+++ panelmodulus/views.py
@@ -2,8 +2,8 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.request import Request
 from rest_framework.response import Response
-
 from .serializers import BusinessmanModulusSerializer
+from common.util import paginators
 
 
 @api_view(['GET'])
@@ -11,6 +11,8 @@ def get_businessman_modulus(request: Request):
 
     data = request.user.businessmanmodulus_set.all()
 
-    serializer = BusinessmanModulusSerializer(data, many=True)
+    # serializer = BusinessmanModulusSerializer(data, many=True)
+    # return Response(serializer.data, status=status.HTTP_200_OK)
 
-    return Response(serializer.data, status=status.HTTP_200_OK)
+    paginate=paginators.NumberedPaginator(request, data, BusinessmanModulusSerializer)
+    return paginate.next_page()
