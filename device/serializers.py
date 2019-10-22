39343cb70c344122c69a3c7fbdb632a4f4473887
hossein otrsh <boyyoung2020@gmail.com>
2019-10-22 23:51:53 +0330

    create api view,serializer for device app

--- device/serializers.py
+++ device/serializers.py
@@ -1,22 +1,43 @@
 from rest_framework import serializers
 from .models import Device
+from users.models import Businessman,Customer
+from django.core.exceptions import ObjectDoesNotExist
 
 
 class CustomerRegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
-        model = Device
+        model = Customer
         fields = [
             'id',
-            'imei',
+
         ]
-        extra_kwargs = {'id': {'read_only': True},
-                        'imei': {'required': True},
-                       }
+        extra_kwargs = {'id': {'read_only': True}}
+
+
+    def validate(self, validate_data):
+        imei_number=self.context['imei_number']
+        phone_customer=self.context['phone_customer']
+
+
+        if Device.objects.filter(imei=imei_number).exists():
+            this_businessman=Businessman.objects.get(device__imei=imei_number)
+
+            if  Customer.objects.filter(phone=phone_customer,businessman=this_businessman).exists():
+
+                raise serializers.ValidationError({'customer': ['customer is existed already']})
+
+            return validate_data
+        else:
+            raise serializers.ValidationError({'device': ['customer is not exist']})
+
 
     def create(self, validated_data):
-        request=self.context['request']
-        return Device.objects.create(businessman=this_businessman,
-                                     phone=this
+        imei_number=self.context['imei_number']
+        phone_customer=self.context['phone_customer']
+
+        this_businessman=Businessman.objects.get(device__imei=imei_number)
+        return Customer.objects.create(businessman=this_businessman,
+                                      phone=phone_customer,
                                       **validated_data,
                                       )
--- device/views.py
+++ device/views.py
@@ -8,30 +8,58 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.generics import GenericAPIView
 from rest_framework.mixins import CreateModelMixin
+from rest_framework.decorators import api_view, permission_classes
 
-
-
-
-
+@api_view(['POST'])
 def register_customer(request,imei_number,phone_customer):
-    if Device.objects.filter(imei=imei_number).exists():
-        this_businessman=Businessman.objects.get(device__imei=imei_number)
+    """
+    Registers new customer. It Needs to be sended phone by url
+    """
 
-        if not Customer.objects.filter(phone=phone_customer,businessman=this_businessman).exists():
-            this_customer = Customer.objects.create(businessman=this_businessman,
-                                                    phone=phone_customer,
-                                                    full_name="test",
-                                                    )
+    serializer = CustomerRegisterSerializer(data=request.data,context={
+                                            "imei_number":imei_number,
+                                            "phone_customer":phone_customer,
+                                            })
 
-            get_id=this_customer.id
-            return HttpResponse("customer_id:"+str(get_id))
+    if not serializer.is_valid():
 
-        else:
-            return HttpResponse("this customer already existed")
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    else:
-        return HttpResponse("FALSE:businessman dont exist")
+    customer = serializer.save()
+
+    return Response(data={'id': customer.id}, status=status.HTTP_201_CREATED)
 
 
-class RegisterCustomer(GenericAPIView,CreateModelMixin):
-    serializer_class=CustomerRegisterSerializer
+
+#
+# def register_customer(request,imei_number,phone_customer):
+#     if Device.objects.filter(imei=imei_number).exists():
+#         this_businessman=Businessman.objects.get(device__imei=imei_number)
+#
+#         if not Customer.objects.filter(phone=phone_customer,businessman=this_businessman).exists():
+#             this_customer = Customer.objects.create(businessman=this_businessman,
+#                                                     phone=phone_customer,
+#                                                     full_name="test",
+#                                                     )
+#
+#             get_id=this_customer.id
+#             return HttpResponse("customer_id:"+str(get_id))
+#
+#         else:
+#             return HttpResponse("this customer already existed")
+#
+#     else:
+#         return HttpResponse("FALSE:businessman dont exist")
+
+
+
+# 
+# class RegisterCustomer(GenericAPIView,CreateModelMixin):
+#     serializer_class=CustomerRegisterSerializer
+#     def get_serializer_context(self):
+#         context={
+#                 "request":request,
+#                 "imei_number":imei_number,
+#                 "phone_customer":phone_customer,
+#         }
+#         return context
