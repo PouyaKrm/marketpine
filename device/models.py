c2ee2946a01ef415f4990bb41e495f7c1a9bab1e
Pouya Kermanshahi <pouya795@outlook.com>
2019-10-26 06:30:08 -0700

    changing type of emie to varchar 20 in device app.

--- device/models.py
+++ device/models.py
@@ -5,5 +5,5 @@ from users.models import Businessman
 class Device(models.Model):
     register_date = models.DateTimeField(auto_now_add=True)
     modification_date = models.DateTimeField(auto_now=True)
-    imei=models.IntegerField()
+    imei = models.CharField(max_length=25)
     businessman=models.OneToOneField(Businessman,on_delete=models.CASCADE)
