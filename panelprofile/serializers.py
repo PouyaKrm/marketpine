e2dfe71cc14b355b891ab80fdcdc0e7b46a6097d
Pouya Kermanshahi <pouya795@outlook.com>
2020-03-01 01:30:57 -0800

    Set has_sms_panel in authentication documents upload.

--- panelprofile/serializers.py
+++ panelprofile/serializers.py
@@ -196,10 +196,7 @@ class AuthSerializer(serializers.ModelSerializer):
 
         password = validated_data.pop('password')
 
-        client_manage = ClientManagement()
-
-
-        if hasattr(user, 'smspanelinfo'):
+        if user.has_sms_panel:
             user.smspanelinfo.update_panel_info()
         else:
             sms = SMSPanelInfo()
