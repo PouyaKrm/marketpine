574fea5007fab0628fe06ee49c9b300be3deb131
Pouya Kermashahi <pouya795@outlook.com>
2020-03-22 02:47:18 +0430

    adds customer_return_plan urls.py file.

---
+++ customer_return_plan/urls.py
@@ -0,0 +1,11 @@
+from .festivals import urls as festival_url
+from .invitation import urls as invitation_url
+
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('dashboard/', views.dashboard_data),
+    path('festival/', include(festival_url)),
+    path('invitation/', include(invitation_url))
+]
