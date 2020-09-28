36273d4a3402d9ccca6550ec4aba530ce6eb7074
Pouya Kermanshahi <pouya795@outlook.com>
2020-09-28 12:40:30 +0330

    Adds MultipartRequestBodyDictFiled in base_app serializers.py

--- base_app/serializers.py
+++ base_app/serializers.py
@@ -1,3 +1,5 @@
+import ast
+
 from rest_framework import serializers
 
 from common.util import create_link
@@ -18,6 +20,7 @@ def pop_request_if_exist(**kwargs):
     if kw.keys().__contains__('request'):
         kw.pop('request')
     return kw
+
 
 def add_request_to_context_if_exist(request, **kwargs):
     kw = kwargs.copy()
@@ -61,4 +64,38 @@ class ImageFiledWithLinkRepresentation(serializers.ImageField):
         if not value:
             return None
         return create_link(value.url, self.context['request'])
+
+
+class MultipartRequestBodyDictFiled(serializers.Field):
+
+    def __init__(self, max_items: int):
+        super().__init__()
+        self.max_items = max_items
+
+    def to_internal_value(self, data) -> dict:
+        if type(data) != str or len(data) > 200:
+            raise serializers.ValidationError("invalid field")
+        try:
+            result = ast.literal_eval(data)
+            if type(result) != dict:
+                raise serializers.ValidationError("inavlid dictionary")
+            return self._validate_items(result)
+        except (SyntaxError, ValueError):
+            raise serializers.ValidationError("invalid field")
+
+    def _validate_items(self, id_show_orders: dict):
+        if len(id_show_orders) > self.max_items:
+            raise serializers.ValidationError("invalid field")
+        parsed_dict = {}
+        for k, v in id_show_orders.items():
+            try:
+                k_int = int(k)
+                v_int = int(v)
+                if k_int <= 0:
+                    raise serializers.ValidationError("{} is invalid id value".format(k_int))
+                parsed_dict[k_int] = v_int
+            except ValueError:
+                raise serializers.ValidationError("field contains un parsable to int values")
+
+        return parsed_dict
 
