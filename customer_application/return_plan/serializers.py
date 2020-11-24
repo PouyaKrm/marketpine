31ce84b6e5516b0900114b3106b3b41166567992
Pouya Kermanshahi <pouya795@outlook.com>
2020-11-24 20:11:13 +0330

    bug fixes in friend invitation

--- customer_application/return_plan/serializers.py
+++ customer_application/return_plan/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from base_app.serializers import BaseModelSerializerWithRequestObj, BaseSerializerWithRequestObj
+from common.util import create_field_error
 from common.util.custom_validators import phone_validator
 from customer_application.serializers import BusinessmanIdRelatedField, BaseBusinessmanSerializer, \
     customer_full_name_field
@@ -12,7 +13,7 @@ class FriendInvitationSerializer(BaseSerializerWithRequestObj):
     businessman = BusinessmanIdRelatedField()
     friend_phone = serializers.CharField(max_length=20, validators=[phone_validator])
     friend_full_name = serializers.CharField(max_length=40)
-    full_name = serializers.CharField(max_length=40)
+    full_name = serializers.CharField(required=False, max_length=40)
 
     class Meta:
         fields = [
@@ -23,6 +24,14 @@ class FriendInvitationSerializer(BaseSerializerWithRequestObj):
         ]
 
         extra_kwargs = {'full_name': {'required': False}}
+
+    def validate(self, attrs):
+        user = self.request.user
+        fn = attrs.get('full_name')
+        if not user.is_full_name_set() and fn is None:
+            err = create_field_error('full_name', ['نام کاربر اجباری است'])
+            raise serializers.ValidationError(err)
+        return attrs
 
     def validate_friend_phone(self, value):
         if value == self.request.user.phone:
