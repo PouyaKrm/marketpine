c87555578573f8e6954c806ea6c0454e0a06130a
Pouya Kermanshahi <pouya795@outlook.com>
2021-02-14 11:56:23 +0330

    Adds permissions.py to base_app and adds AllowAnyOnGet permission to it

---
+++ base_app/permissions.py
@@ -0,0 +1,11 @@
+from django.views.generic.base import View
+from rest_framework.permissions import BasePermission
+from rest_framework.request import Request
+
+
+class AllowAnyOnGet(BasePermission):
+
+    def has_permission(self, request: Request, view: View) -> bool:
+        if request.method == 'GET':
+            return True
+        return request.user.is_authenticated()
