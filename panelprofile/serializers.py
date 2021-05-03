c72e1f19a32824dac41f92f32abe627ab496129d
Pouya Kermanshahi <pouya795@outlook.com>
2021-05-03 16:06:41 +0430

    Adds is_phone_verified field to profile serializer

--- panelprofile/serializers.py
+++ panelprofile/serializers.py
@@ -57,6 +57,7 @@ class BusinessmanProfileSerializer(BaseModelSerializerWithRequestObj):
             'business_category',
             'business_name',
             'date_joined',
+            'is_phone_verified',
             'authorized',
             'has_sms_panel',
             'panel_activation_date',
