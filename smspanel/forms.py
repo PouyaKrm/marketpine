0ad84687b1926d7fcad5dea3d3cfa5ba8cf2063e
Pouya Kermanshahi <pouya795@outlook.com>
2021-05-19 17:09:50 +0430

    Updates smspanel forms.py to use SmsPanelInfoService

--- smspanel/forms.py
+++ smspanel/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 
 from panelprofile.models import SMSPanelStatus
+from panelprofile.services import sms_panel_info_service
 from .models import SMSMessage
 
 
@@ -13,6 +14,7 @@ class SMSMessageForm(forms.ModelForm):
     def clean(self):
         super().clean()
         businessman = self.cleaned_data.get('businessman')
-        if businessman is None or not businessman.has_sms_panel or businessman.smspanelinfo.status == SMSPanelStatus.INACTIVE:
-            raise forms.ValidationError('businessman has no sms panel or sms panel is inactive', code='error')
+        has_panel_and_is_active = sms_panel_info_service.fetch_panel_and_check_is_active(businessman)
+        if not has_panel_and_is_active:
+            raise forms.ValidationError('کاربر پنل پیامک ندارد یا پنل پیامکش غیر فعال است', code='error')
 
