ff54db41d1b83bb8c35c0d1335ebfe61924def8c
Pouya Kermanshahi <pouya795@outlook.com>
2021-06-16 20:46:29 +0430

    commits left file from previous commit

--- customer_application/return_plan/views.py
+++ customer_application/return_plan/views.py
@@ -1,5 +1,6 @@
+import logging
+
 from rest_framework.decorators import api_view, authentication_classes
-from rest_framework.pagination import PageNumberPagination
 from rest_framework.request import Request
 
 from common.util.http_helpers import bad_request, ok
@@ -9,8 +10,7 @@ from customer_application.pagination import CustomerAppListPaginator
 from customer_application.return_plan.serializers import FriendInvitationSerializer, CustomerReadonlyDiscountSerializer
 from customer_application.return_plan.services import return_plan_service, InvitationInfo
 from customer_application.services import customer_data_service
-from customer_return_plan.services import customer_discount_service, discount_service
-import logging
+from customer_return_plan.services import customer_discount_service
 
 logger = logging.getLogger(__name__)
 
@@ -34,17 +34,21 @@ def friend_invitation(request: Request):
         return bad_request(e.http_message)
 
 
-
 class CustomerDiscountListAPIView(BaseListAPIView):
 
     def get_queryset(self):
         ust = self.request.query_params.get('ust', 'available')
+        businessman_id = self.request.query_params.get('businessman')
         if ust == 'used':
             query = customer_discount_service.get_customer_used_discounts(self.request.user)
         else:
             query = customer_discount_service.get_customer_available_discount(self.request.user)
-        # return customer_discount_service.get_customer_available_discount(self.request.user)
-        return query
+
+        if businessman_id is not None and businessman_id.isnumeric() and int(businessman_id) > 0:
+            query = query.filter(businessman__id=int(businessman_id))
+
+        return query.order_by('-create_date')
+
     serializer_class = CustomerReadonlyDiscountSerializer
 
 
@@ -58,9 +62,13 @@ def customer_businessman_discounts_list_api_view(request: Request, businessman_i
     try:
         businessman = customer_data_service.get_businessman_by_id_or_page_id(businessman_id)
         if ust == 'used':
-            discounts = discount_service.get_customer_used_discounts_for_businessman(businessman, customer)
+            discounts = customer_discount_service.get_customer_used_discounts(request.user).filter(
+                businessman=businessman)
+            # discounts = discount_service.get_customer_used_discounts_for_businessman(businessman, customer)
         else:
-            discounts = discount_service.get_customer_available_discounts_for_businessman(businessman, customer)
+            # discounts = discount_service.get_customer_available_discounts_for_businessman(businessman, customer)
+            discounts = customer_discount_service.get_customer_available_discount(customer).filter(
+                businessman=businessman)
         result_page = p.paginate_queryset(discounts, request)
         sr = CustomerReadonlyDiscountSerializer(result_page, request=request, many=True)
         return p.get_paginated_response(sr.data)
