15718e288a8a2078581f74769dbcf963132e5f25
Pouya Kermanshahi <pouya795@outlook.com>
2021-06-24 20:23:29 +0430

    Adds wallet model to admin

--- payment/admin.py
+++ payment/admin.py
@@ -1,19 +1,25 @@
 from django.contrib import admin
 
 from .forms import PanelActivationForm
-from .models import Payment, FailedPaymentOperation, PanelActivationPlans
+from .models import Payment, FailedPaymentOperation, PanelActivationPlans, Wallet
 
 
 class PaymentAdmin(admin.ModelAdmin):
     fields = ("businessman", "authority", "refid",
               "create_status", "description", "phone", "amount", 'panel_plan', 'call_back_status')
-    list_display = ('businessman', 'payment_type', 'create_status', 'verification_status', 'creation_date', 'authority', 'refid')
+    list_display = (
+        'businessman', 'payment_type', 'create_status', 'verification_status', 'creation_date', 'authority', 'refid')
     list_filter = ('create_status', 'businessman')
 
+
+class WalletModelAdmin(admin.ModelAdmin):
+    list_display = ['businessman', 'available_credit', 'used_credit', 'last_credit_increase_date', 'create_date']
+    list_display_links = ['businessman', 'available_credit', 'used_credit', 'last_credit_increase_date', 'create_date']
 
 
 class PaymentOperationFailedAdmin(admin.ModelAdmin):
     list_display = ('businessman', 'operation_type', 'create_date', 'payment_amount', 'is_fixed')
+
 
 class PanelActiovationPalnModelAdmin(admin.ModelAdmin):
     list_display = ['price_in_toman', 'duration_type', 'title', 'duration']
@@ -23,3 +29,4 @@ class PanelActiovationPalnModelAdmin(admin.ModelAdmin):
 admin.site.register(Payment, PaymentAdmin)
 admin.site.register(FailedPaymentOperation, PaymentOperationFailedAdmin)
 admin.site.register(PanelActivationPlans, PanelActiovationPalnModelAdmin)
+admin.site.register(Wallet, WalletModelAdmin)
