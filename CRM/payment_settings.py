02e2590da71498dcca76119b427584a67e2a71f5
Pouya Kermanshahi <pouya795@outlook.com>
2021-07-18 12:47:16 +0430

    Changing CALLBACK_URL in ZRINPAL settings

--- CRM/payment_settings.py
+++ CRM/payment_settings.py
@@ -16,5 +16,5 @@ ZARINPAL = {
     'url': 'https://www.zarinpal.com/pg/services/WebGate/wsdl',
     "MERCHANT": "7055b6ac-e6dc-11e9-99c1-000c295eb8fc",
     "FORWARD_LINK": "https://www.zarinpal.com/pg/StartPay/{}/ZarinGate",  # use this string with .format method
-    "CALLBACK_URL": '{}/payment/verify'.format(settings.FRONTEND_URL)
+    "CALLBACK_URL": '{}/payment/result'.format(settings.FRONTEND_URL)
 }
