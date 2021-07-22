c997284e1e300c9525aed4e0b818fe4cdb82f305
Pouya Kermanshahi <pouya795@outlook.com>
2021-07-22 19:11:27 +0430

    Changes CustomerLoyaltyDiscountSettings model

--- customer_return_plan/loyalty/models.py
+++ customer_return_plan/loyalty/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-from customer_return_plan.models import BaseDiscountSettings
 
+from customer_return_plan.models import BaseDiscountSettings
 # Create your models here.
-from users.models import BaseModel, BusinessmanOneToOneBaseModel
+from users.models import BaseModel, BusinessmanOneToOneBaseModel, BusinessmanManyToOneBaseModel
 
 
 class CustomerPurchaseNumberDiscountSettings(BaseModel, BaseDiscountSettings):
@@ -13,28 +13,10 @@ class CustomerPurchaseAmountDiscountSettings(BaseModel, BaseDiscountSettings):
     purchase_amount = models.PositiveIntegerField(default=0)
 
 
-class CustomerLoyaltyDiscountSettings(BusinessmanOneToOneBaseModel):
+class CustomerLoyaltyDiscountSettings(BaseDiscountSettings, BusinessmanManyToOneBaseModel):
+    point = models.PositiveIntegerField(default=0)
+    discount_code = models.CharField(max_length=20, null=True)
 
-    FOR_PURCHASE_AMOUNT = '0'
-    FOR_PURCHASE_NUMBER = '1'
-    FOR_BOTH = '2'
-
-    use_choices = [
-        (FOR_PURCHASE_AMOUNT, 'Purchase Amount'),
-        (FOR_PURCHASE_NUMBER, 'Purchase Number'),
-        (FOR_BOTH, 'Both')
-    ]
-
-    purchase_number_settings = models.OneToOneField(CustomerPurchaseNumberDiscountSettings, on_delete=models.CASCADE, null=True)
-    purchase_amount_settings = models.OneToOneField(CustomerPurchaseAmountDiscountSettings, on_delete=models.CASCADE, null=True)
-    create_discount_for = models.CharField(max_length=2, choices=use_choices, default=FOR_BOTH)
-    disabled = models.BooleanField(default=False)
-
-    def is_discount_for_purchase_amount(self) -> bool:
-        return self.create_discount_for == CustomerLoyaltyDiscountSettings.FOR_PURCHASE_AMOUNT
-
-    def is_discount_for_purchase_number(self) -> bool:
-        return self.create_discount_for == CustomerLoyaltyDiscountSettings.FOR_PURCHASE_NUMBER
-
-    def is_discount_for_both(self) -> bool:
-        return self.create_discount_for == CustomerLoyaltyDiscountSettings.FOR_BOTH
+    class Meta:
+        db_table = 'loyalty_settings'
+        unique_together = [('businessman', 'point')]
